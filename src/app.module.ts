bfc6b29eef4ad3e95fe998579a33039b26c661de
max <max@circle-vn.com>
2026-01-13 02:44:54 +0700

    feat: add AuthModule and UsersModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getTypeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { getTypeOrmConfig } from './config/typeorm.config';
       inject: [ConfigService],
       useFactory: getTypeOrmConfig,
     }),
+    AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
